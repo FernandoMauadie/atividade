396d7ca0a93b5614e65e6e3524e0787a4f973ea1
FernandoMauadie <49196515+FernandoMauadie@users.noreply.github.com>
2019-05-14 10:08:02 -0300

    Update corpo exe 1.py

--- atividade14 _05_2019/corpo exe 1.py
+++ atividade14 _05_2019/corpo exe 1.py
@@ -1,6 +1,6 @@
-import listaG as L
+import listaG as L#import da função
 
-print('1 - Salgado, 2 - Refrigerante, 3- Suco, 4 - Sorvete, 5 - Cafe , 6 - Capuccino , 7 - Bolo, 8 - Dadinho')
-a = int(input('coloque o numero do produto'))
-L.prod(a)
-print('valor do produto:',L.prod(a))
+print('1 - Salgado, 2 - Refrigerante, 3- Suco, 4 - Sorvete, 5 - Cafe , 6 - Capuccino , 7 - Bolo, 8 - Dadinho')#numero dos produtos
+a = int(input('coloque o numero do produto'))#colocar o numero do produto
+L.prod(a)#ultiliza a função prod com o parametro sendo a
+print('valor do produto:',L.prod(a))#imprime o valor do produto
