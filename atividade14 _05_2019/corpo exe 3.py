31db28887316067edf7a699298a8ca027fa5d202
FernandoMauadie <49196515+FernandoMauadie@users.noreply.github.com>
2019-05-14 10:20:41 -0300

    Update corpo exe 3.py

--- atividade14 _05_2019/corpo exe 3.py
+++ atividade14 _05_2019/corpo exe 3.py
@@ -1,5 +1,5 @@
-import listaG as L
+import listaG as L#import a função
 
-print('coloque o nome do cliente, o nome do produto e seu numero: 1 - Salgado, 2 - Refrigerante, 3- Suco, 4 - Sorvete, 5 - Cafe , 6 - Capuccino , 7 - Bolo, 8 - Dadinho')
-p = L.ListaGN()
-print(p)
+print('coloque o nome do cliente, o nome do produto e seu numero: 1 - Salgado, 2 - Refrigerante, 3- Suco, 4 - Sorvete, 5 - Cafe , 6 - Capuccino , 7 - Bolo, 8 - Dadinho')#numero dos produtos
+p = L.ListaGN()#ultiliza a função ListaGN
+print(p)#imprime a variavel p
